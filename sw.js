67f0521656ac02bb8ae524a6ead5a5aa8503f23b
asher <100488394+ashertenenbaum@users.noreply.github.com>
2026-03-24 18:31:32 +1300

    Refactor push notification handling in sw.js
    Updated notification title and removed unnecessary comments.

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
 self.addEventListener('push', function(event) {
-    // Generate the random money amount locally when the ping arrives!
+    // Generate the random money amount locally
     const amount = (Math.random() * (50 - 1) + 1).toFixed(2);
     
     const options = {
         body: `You received a payment of ${amount} NZD from a customer.`,
         icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGluJhW7I1NYU7jF77E-9K9I46_ib_DUNHw&s',
-        badge: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGluJhW7I1NYU7jF77E-9K9I46_ib_DUNHw&s',
-        // 'requireInteraction' keeps the notification on screen until you dismiss it
-        requireInteraction: true 
+        badge: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQGluJhW7I1NYU7jF77E-9K9I46_ib_DUNHw&s'
     };
 
-    // We changed the title so iOS naturally adds "from Stripe" underneath it
+    // '\u200b' is an invisible, zero-width space character!
     event.waitUntil(
-        self.registration.showNotification('Payment Received', options)
+        self.registration.showNotification('\u200b', options)
     );
 });
 
